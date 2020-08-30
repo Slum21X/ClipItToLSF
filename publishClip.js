a50b6be49d902fab599348696f0ff11237bf342a
Slum21X <slum21x@gmail.com>
2020-08-30 20:20:29 +0200

    ensure title exists during publish event

--- publishClip.js
+++ publishClip.js
@@ -1,7 +1,13 @@
+/*jshint esversion: 6 */
 
 function publish(event){
   if (event.target.getAttribute("data-a-target") == "tw-core-button-label-text"){
-    console.log("helloooooooooooooooooooooooo");
+
+    let title = document.getElementById("cmgr-title-input").getAttribute("value");
+
+    if (title.length > 0 && title.length <= 100){
+      console.log("helloooooooooooooooooooooooo");
+    }
   }
 }
 
