2de56f03f0f861771639680c0e0f91203668c5bd
Slum21X <slum21x@gmail.com>
2020-09-01 13:50:48 +0200

    fix an error from using multiple optional chaining in a row

--- background.js
+++ background.js
@@ -2,25 +2,29 @@
 
 function getSlugAndTitle(details){
 
-  const body8Array = details.requestBody?.raw['0']?.bytes // jshint ignore:line
+  if (details.requestBody?.raw != null){ // jshint ignore:line
+    const body8Array = details.requestBody.raw['0']?.bytes // jshint ignore:line
 
-  if (body8Array != null){ // jshint ignore:line
+    if (body8Array != null){ // jshint ignore:line
 
-    const requestBodyString = String.fromCharCode.apply(null, new Uint8Array(body8Array));
-    const parsedBody = JSON.parse(requestBodyString)['0'];
+      const requestBodyString = String.fromCharCode.apply(null, new Uint8Array(body8Array));
+      const parsedBody = JSON.parse(requestBodyString);
 
-    if (parsedBody != null){
-      if (parsedBody.operationName == "PublishClip"){
+      if (parsedBody != null){
+        if (parsedBody.operationName == "PublishClip"){
 
-        const slug = parsedBody.variables.input.slug;
-        const title = parsedBody.variables.input.title;
-        const url = "https://clips.twitch.tv/" + slug;
+          const inputs = parsedBody['0'].variables.input;
 
-        chrome.tabs.create({
-          url: "https://www.reddit.com/r/LivestreamFail/submit" +
-          "?url=" + url +
-          "&title=" + title
-        });
+          const slug = inputs.slug;
+          const title = inputs.title;
+          const url = "https://clips.twitch.tv/" + slug;
+
+          chrome.tabs.create({
+            url: "https://www.reddit.com/r/LivestreamFail/submit" +
+            "?url=" + url +
+            "&title=" + title
+          });
+        }
       }
     }
   }
